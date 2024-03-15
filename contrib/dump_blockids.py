5e18d67e616270528747e6a8f1828bc7c9d4e2af
Simon Walker <github@stwalkerster.co.uk>
2024-03-15 18:37:50 +0000

    Add script to parse out all allocated block IDs
    This should make it easier to detect any double-allocated block IDs that
    should be rendering differently.

---
+++ contrib/dump_blockids.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+"""
+Outputs a list of all allocated block IDs and which namespaced block names are associated with them.
+
+All blocks which share a (block id, data id) tuple will sit in the same array. Different data IDs go onto different
+lines. There should be no unrelated blocks (from a rendering perspective) on the same line.
+"""
+
+from collections import OrderedDict
+from overviewer_core import world
+
+
+# noinspection PyProtectedMember
+def main():
+    w = world.RegionSet('.', '.')
+    dataset = OrderedDict()
+
+    for mc_id in w._blockmap:
+        block_id = w._blockmap[mc_id][0]
+        block_data = w._blockmap[mc_id][1]
+
+        if block_id not in dataset:
+            dataset[block_id] = OrderedDict()
+
+        if block_data not in dataset[block_id]:
+            dataset[block_id][block_data] = []
+
+        dataset[block_id][block_data].append(mc_id)
+
+        pass
+
+    print("# All Overviewer block IDs to Minecraft namespaced block IDs")
+    print("```")
+    last = -1
+    for block_id in OrderedDict(sorted(dataset.items())):
+        if (last + 1) < block_id:
+            print("\n-- unallocated space --\n")  # empty line to show a gap in numbering
+        last = block_id
+
+        item_set = [x[1] for x in dataset[block_id].items()]
+
+        if len(item_set) == 1:
+            print(("%d" % block_id).ljust(6), item_set[0])
+        else:
+            print(("%d" % block_id).ljust(6))
+            for s in item_set:
+                print("".ljust(6), s)
+
+    print("```")
+    print()
+    print("# Data conflicts:")
+    print("```")
+    for block_id in dataset:
+        for block_data in dataset[block_id]:
+            if len(dataset[block_id][block_data]) > 1:
+                print("(%d, %d)" % (block_id, block_data), dataset[block_id][block_data])
+
+    print("```")
+
+
+if __name__ == '__main__':
+    main()
